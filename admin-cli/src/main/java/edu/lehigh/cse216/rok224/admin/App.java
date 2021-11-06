a550fd566e536ede43a0a558641cd1a24265f7f4
Adyn Gallagher <arg422@lehigh.edu>
2021-11-06 18:48:34 -0400

    removed add/drop tables from admin cli

--- admin-cli/src/main/java/edu/lehigh/cse216/rok224/admin/App.java
+++ admin-cli/src/main/java/edu/lehigh/cse216/rok224/admin/App.java
@@ -18,22 +18,15 @@ public class App {
      */
     static void menu() {
         System.out.println("Main Menu");
-        System.out.println("  [T] Create tblData");
-        System.out.println("  [D] Drop tblData");
-        System.out.println("  [1] Query for a specific row");
-        System.out.println("  [*] Query for all rows");
+        System.out.println("  [T] View all tables");
+        System.out.println("  [1] Query for a specific message");
+        System.out.println("  [*] Query for all messages");
         System.out.println("  [-] Delete a row");
         System.out.println("  [+] Insert a new row");
         System.out.println("  [~] Update a row");
         System.out.println("  [q] Quit Program");
         System.out.println("  [?] Help (this message)");
-        System.out.println("  [i] Increment likes for a specific row");
-        System.out.println("  [d] Decrement likes for a specific row");
 
-        System.out.println("  [L] Create likes table");
-        System.out.println("  [M] Create messages table");
-        System.out.println("  [C] Create comments table");
-        System.out.println("  [U] Create user table");
     }
 
     /**
@@ -137,52 +130,28 @@ public class App {
                 menu();
             } else if (action == 'q'){                      // quit --------------------------------------
                 break;
-            } else if (action == 'T'){                      // create table --------------------------------------
-                int res = db.createTable();
-                if(res == -1){
-                    continue;
-                }
-                System.out.println("Table has been created");
-            } else if (action == 'D'){                      // drop table --------------------------------------
-                int res = db.dropTable();
-                if(res == -1){
-                    continue;
-                }
-                System.out.println("Table has been dropped");
             } else if (action == '1'){                      // query specific row --------------------------------------
                 int id = getInt(in, "Enter the row ID");
                 if(id == -1){
                     continue;
                 } 
-                Database.RowData res = db.selectOne(id);
-                if(res != null) {
-                    System.out.println(" ID [" + res.mId + "] ");
-                    System.out.println(" Message --> " + res.mMessage);
-                    System.out.println(" Likes --> " + res.mLikes);
-                }
+                Database.Message res = db.selectMessage(id);
+                System.out.println(res);
             } else if(action == '*') {                      // query for all rows --------------------------------------
-                ArrayList<Database.RowData> res = db.selectAll();
+                ArrayList<Database.Message> res = db.selectAllMessages();
                 if (res == null){
                     continue;
                 }
-                System.out.println("  Current Database Contents");
+                System.out.println("  Current Message Contents");
                 System.out.println("  -------------------------");
-                for (Database.RowData rd : res){
-                    System.out.println(" ID --> [" + rd.mId + "] ");
-                    System.out.println(" Message --> [" + rd.mMessage + "] ");
-                    System.out.println(" Likes --> [" + rd.mLikes + "] ");
-                }
+                System.out.println(res);
             } else if(action == '-') {                      // delete a row --------------------------------------
-                int id = getInt(in, "Enter the row ID");
+                int id = getInt(in, "Enter the message ID");
                 if (id == -1){
                     continue;
                 }
-                String table = getString(in, "Enter table name");
-                if ( !(table.equals("user") || table.equals("comment") || table.equals("likes") || table.equals("message") )) {
-                    continue;
-                }
 
-                int res = db.deleteRow(id, table);
+                int res = db.deleteMessage(id);
                 if(res == -1){
                     continue;
                 }
@@ -193,63 +162,16 @@ public class App {
                     System.out.println("Can't make an empty message");
                     continue;
                 }
+                System.out.println("+ currently unimplemented");
 
-                
-
-
-                int res = db.insertRow(message, 0);
-                System.out.println(res + " rows added");
+                // int res = db.insertMessage("admin", 0);
+                // System.out.println(res + " rows added");
             } else if(action == '~'){                       // update a row --------------------------------------
                 int id = getInt(in, "Enter the row ID :> ");
                 if (id == -1){
                     continue;
                 }
-                String newMessage = getString(in, "Enter the new message");
-                int res = db.updateOne(id, newMessage);
-                if (res == -1){
-                    continue;
-                }
-                System.out.println("  " + res + " rows updated");
-            } else if (action == 'i'){                      // increment likes for a specific row --------------------------------------
-                int id = getInt(in, "Enter the row ID :> ");
-                if(id == -1){
-                    continue;
-                }
-                db.incrementLikes(id);
-            } else if (action == 'd'){                      // decrement likes for a specific row --------------------------------------
-                int id = getInt(in, "Enter the row ID :> ");
-                if(id == -1){
-                    continue;
-                }
-                int res = db.decrementLikes(id);
-                if(res == -1){
-                    continue;
-                }
-                System.out.println("Row has been updated");
-            } else if (action == 'L') {                     // create likes table --------------------------------------
-                int res = db.createLikesTable();
-                if(res == -1){
-                    continue;
-                }
-                System.out.println("Likes table has been created");
-            } else if (action == 'M') {                     // create message table --------------------------------------
-                int res = db.createMsgTable();
-                if(res == -1){
-                    continue;
-                }
-                System.out.println("Message table has been created");
-            } else if (action == 'C') {                     // create comments table
-                int res = db.createCommentsTable();
-                if(res == -1){
-                    continue;
-                }
-                System.out.println("Comments table has been created");
-            } else if (action == 'U') {                     // create user table
-                int res = db.createUserTable();
-                if(res == -1){
-                    continue;
-                }
-                System.out.println("User table has been created");
+                System.out.println("+ currently unimplemented");
             }
         }
         // Always remember to disconnect from the database when the program 
